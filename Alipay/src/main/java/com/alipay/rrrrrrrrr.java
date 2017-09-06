b0dbd6121504efed00acffd5e1b31437c3a36ef6
ganjunjie <1396695357@qq.com>
2017-09-06 10:41:14 +0800

    dd

---
+++ Alipay/src/main/java/com/alipay/rrrrrrrrr.java
@@ -0,0 +1,15 @@
+package com.alipay;
+
+/**
+ * <dl>
+ * <dt>rrrrrrrrr</dt>
+ * <dd>Description:类描述</dd>
+ * <dd>Copyright: 版权归杭州京歌科技有限公司所有</dd>
+ * <dd>Company: 杭州京歌科技有限公司</dd>
+ * <dd>CreateDate: 2017/9/6</dd>
+ * </dl>
+ *
+ * @author 小杰
+ */
+public class rrrrrrrrr {
+}
